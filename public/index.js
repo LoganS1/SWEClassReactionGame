0b536e2e429d5e5ac46af6d83aacc241999ea9d5
Drew <andrewmurray@ou.edu>
2023-03-27 16:41:10 -0500

    setup preliminary login page, still need to style and connect to database.

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
-document.write("written by js file");
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "password") {
+        location.href="http://localhost:8010/game";
+    } else {
+        alert("Incorrect username and/or password");
+        //location.reload();
+    }
+})
