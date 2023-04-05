f9d381bd89eb220243057aca09a54b53afd9b9fd
Drew <andrewmurray@ou.edu>
2023-04-04 23:43:16 -0500

    login now sends user to game webpage.

--- public/index.js
+++ public/index.js
@@ -3,12 +3,23 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 const signupButton = document.getElementById("sign-up-form");
 
-loginButton.addEventListener("click", (e) => {
+loginButton.addEventListener("click", async (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "password") {
+    let response = await fetch('/validateLogin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+
+    });
+
+    let data = await response.json();
+
+    if (data.validated) {
         location.href="http://localhost:8010/game";
     } else {
         alert("Incorrect username and/or password");
